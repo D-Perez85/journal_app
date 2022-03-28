6ec535274a9b032336986d9fae03fb7b7929590b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-28 20:27:09 -0300

    Notes Components + SASS

---
+++ src/components/notes/NoteScreen.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import NotesAppBar from "./NotesAppBar";
+
+const NoteScreen = () => {
+  return (
+    <div className="notes__main-content">
+      <NotesAppBar />
+      <div className="notes__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          name="title"
+          className="notes__title-input"
+        />
+        <textarea
+          placeholder="what happend today"
+          className="notes__text-area">
+        </textarea>
+ 
+          <div className="notes__image">
+            {/* <img src="" alt="vista"></img> */}
+            <h4>IMAGE</h4>
+          </div>
+      </div>
+      <button className="btn btn-danger">
+        Delete
+      </button>
+    </div>
+  );
+};
+export default NoteScreen;
---
+++ src/components/notes/NotesAppBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotesAppBar = () => {
+  return (
+    <div className='notes__appBar'>
+        <span>2 de agosto 2020</span>
+        <div>
+            <button className='btn'> Picture </button>
+            <button className='btn'>Save</button>
+        </div>
+    </div>
+  )
+}
+export default NotesAppBar
